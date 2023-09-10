58d84ba8598ba8f6f3beaee1802c4e2603b01938
Bechir <bechirguerriche@gmail.com>
2023-09-10 22:08:49 +0100

    edit axios

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const request = axios.create({
-    baseURL:"http://localhost:8000" 
-        //baseURL:"https://parisian-makeup-inventory-app-api.onrender.com" *//
+    baseURL:"https://parisian-makeup-inventory-app-api.onrender.com" 
 
 });
 
